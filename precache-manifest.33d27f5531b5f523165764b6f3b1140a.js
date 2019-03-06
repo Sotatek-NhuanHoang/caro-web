1df60f9769fce0bb3e01d6ea3ae01198d16a3a44
Sotate-NhuanHoang <nhuan.hoang@sotatek.com>
2019-03-06 17:00:12 +0700

    Release

---
+++ precache-manifest.33d27f5531b5f523165764b6f3b1140a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "daaada351f5d0c1f3cf61bdadaa954f5",
+    "url": "/static/media/o.daaada35.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2485f9260c36ecceb07f",
+    "url": "/static/js/main.2485f926.chunk.js"
+  },
+  {
+    "revision": "63e2152f3bf83fe80b45",
+    "url": "/static/js/2.63e2152f.chunk.js"
+  },
+  {
+    "revision": "2485f9260c36ecceb07f",
+    "url": "/static/css/main.2fe1149b.chunk.css"
+  },
+  {
+    "revision": "63e2152f3bf83fe80b45",
+    "url": "/static/css/2.7d2bfa1c.chunk.css"
+  },
+  {
+    "revision": "f2aa3b503dcf9fbb4ebbf11294cf5fdd",
+    "url": "/index.html"
+  }
+];
